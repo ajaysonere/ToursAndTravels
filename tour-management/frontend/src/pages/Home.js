31ac2acaf9751429743ede217d5664669678ef34
Ajay Sonere <ajaysonere786@gmail.com>
2023-08-03 15:22:49 +0530

    hero is completed

--- tour-management/frontend/src/pages/Home.js
+++ tour-management/frontend/src/pages/Home.js
@@ -1,7 +1,50 @@
+import '../styles/home.css';
+import React from 'react';
+import { Container ,Row , Col} from 'reactstrap';
+import heroImg from '../assets/images/hero-img01.jpg';
+import heroImg02 from '../assets/images/hero-img02.jpg';
+import heroVideo from '../assets/images/hero-video.mp4';
+import SubTitle from '../shared/SubTitle.js';
+
+import worldImg from '../assets/images/world.png';
+
 function Home(){
     return (
        <>
-          <div>Home</div>
+       {/* Hero section start */}
+          <section>
+            <Container>
+               <Row>
+                  <Col lg="6">
+                     <div className='hero_content'>
+                        <div className='hero_subtitle d-flex align-items-center'>
+                           <SubTitle subtitle="Know Before You Go" />
+                           <img src={worldImg} alt='wa'/>
+                        </div>
+                        <h1>Traveling opens the door to creating {" "}<span className='highlights'>memories</span> </h1>
+                         <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. 
+                        </p>
+                     </div>
+                  </Col>
+
+                  <Col lg='2'>
+                     <div className='hero_img-box'>
+                        <img src={heroImg} alt=''></img>
+                     </div>
+                  </Col>
+                  <Col lg='2'>
+                     <div className='hero_img-box mt-4'>
+                        <video src={heroVideo} alt='' controls></video>
+                     </div>
+                  </Col>
+                  <Col lg='2'>
+                     <div className='hero_img-box mt-5'>
+                        <img src={heroImg02} alt=''></img>
+                     </div>
+                  </Col>
+               </Row>
+            </Container>
+          </section>
        </>
     );
 }
---
+++ tour-management/frontend/src/shared/SubTitle.js
@@ -0,0 +1,10 @@
+import React from "react";
+function SubTitle({subtitle}){
+    return(
+        <>
+          <h3 className="section__subtitle">{subtitle}</h3>
+        </>
+    );
+}
+
+export default SubTitle;
