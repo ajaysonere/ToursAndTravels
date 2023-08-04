fefe3b7d94b5472d5791decacf260c46bd7e934f
Ajay Sonere <ajaysonere786@gmail.com>
2023-08-04 17:53:40 +0530

    home page is completed

--- tour-management/frontend/src/components/Footer/Footer.js
+++ tour-management/frontend/src/components/Footer/Footer.js
@@ -1,8 +1,133 @@
+import './../../styles/footer.css';
+import {Container , Row , Col , ListGroup , ListGroupItem} from 'reactstrap';
+import {Link} from 'react-router-dom';
+import logo from './../../assets/images/logo.png';
+
+
+const quick_links =[
+  {
+    path:'/home',
+    display:'Home'
+  },
+  {
+   path:'/about',
+   display:'About'
+  },
+  {
+    path:'/tours',
+    display:'Tours'
+  },
+]
+
+const quick_links2=[
+  {
+    path:'/gallery',
+    display:'Gallery'
+  },
+  {
+   path:'/login',
+   display:'Login'
+  },
+  {
+    path:'/register',
+    display:'Register'
+  },
+]
+
 function Footer(){
+    const year = new Date().getFullYear();
     return(
-        <>
-          <div>Footer</div>
-        </>
+        
+        <footer className='footer'>
+          <Container>
+            <Row>
+              <Col lg='3'>
+                 <div className='logo'>
+                  <img src={logo} alt='logo'/>
+                  <p>Lorem ipsum dolor sit amet consectetur adipsisicing elit .commodi enim. </p>
+                  <div className='social_links d-flex align-items-center gap-4'>
+                     <span>
+                      <Link to='https://github.com/ajaysonere'><i class='ri-github-fill'/></Link>
+                     </span>
+
+                     <span>
+                      <Link to='#'><i class='ri-instagram-line'/></Link>
+                     </span>
+
+                     <span>
+                      <Link to='#'><i class='ri-youtube-line'/></Link>
+                     </span>
+
+                     <span>
+                      <Link to='#'><i class='ri-facebook-circle-line'/></Link>
+                     </span>
+                  </div>
+                 </div>
+              </Col >
+              <Col lg='3'>
+                <h5 className='footer_link-title'>
+                   Discover
+                </h5>
+                <ListGroup className='footer_quick-links'>
+                    {
+                      quick_links.map((item,index)=>(
+                        <ListGroupItem key={index} className='ps-0 border-0'>
+                          <Link to={item.path}>{item.display}</Link>
+                        </ListGroupItem>
+                      ))
+                    }
+                </ListGroup>
+              </Col>
+              <Col lg='3'>
+                <h5 className='footer_link-title'>
+                   Quick Links
+                </h5>
+                <ListGroup className='footer_quick-links'>
+                    {
+                      quick_links2.map((item,index)=>(
+                        <ListGroupItem key={index} className='ps-0 border-0'>
+                          <Link to={item.path}>{item.display}</Link>
+                        </ListGroupItem>
+                      ))
+                    }
+                </ListGroup>
+              </Col>
+              <Col lg='3'>
+                <h5 className='footer_link-title'>
+                   Contact
+                </h5>
+                <ListGroup className='footer_quick-links'>
+                  <ListGroupItem  className='ps-0 border-0 d-flex align-items-center gap-3'>
+                    <h6 className='mb-0 d-flex align-items-center gap-2'>
+                      <span><i class="ri-map-pin-line"></i></span>
+                      Address :
+                    </h6>
+                    <p className='mb-0'>Indore , Madhya Pradesh</p>
+                  </ListGroupItem>
+
+                  <ListGroupItem  className='ps-0 border-0 d-flex align-items-center gap-3'>
+                    <h6 className='mb-0 d-flex align-items-center gap-2'>
+                      <span><i class="ri-mail-line"></i></span>
+                      Email :
+                    </h6>
+                    <p className='mb-0'>ajaysonere786@gmail.com</p>
+                  </ListGroupItem>
+
+                  <ListGroupItem  className='ps-0 border-0 d-flex align-items-center gap-3'>
+                    <h6 className='mb-0 d-flex align-items-center gap-2'>
+                      <span><i class="ri-phone-fill"></i></span>
+                      Phone :
+                    </h6>
+                    <p className='mb-0'>9977449230</p>
+                  </ListGroupItem>
+                </ListGroup>
+              </Col>
+              <Col lg='12' className='text-center pt-5'>
+                <p className='copyright'>Copyright {year}, design and develop by Ajay Sonere </p>
+              </Col>
+            </Row>
+          </Container>
+        </footer>
     );
 }
 
--- tour-management/frontend/src/components/Header/Header.js
+++ tour-management/frontend/src/components/Header/Header.js
@@ -1,5 +1,5 @@
 import './header.css';
-import React from 'react';
+import React,{useRef , useEffect} from 'react';
 import {Container , Row , Button} from 'reactstrap';
 import {NavLink , Link} from 'react-router-dom';
 import logo from '../../assets/images/logo.png';
@@ -20,9 +20,27 @@ const nav_links=[
 ]
 
 function Header(){
+
+    const headerRef = useRef(null);
+
+    const stickyHeaderFunc = ()=>{
+       window.addEventListener('scroll' , ()=>{
+          if(document.body.scrollTop > 80 || document.documentElement.scrollTop > 80){
+             headerRef.current.classList.add('sticky_header');
+          }else{
+            headerRef.current.classList.remove('sticky_header');
+          }
+       })
+    }
+
+    useEffect(()=>{
+      stickyHeaderFunc();
+      return window.removeEventListener('scroll' , stickyHeaderFunc);
+    })
+
     return(
            <>
-              <header className='header'>
+              <header className='header' ref={headerRef}>
                 <Container>
                   <Row>
                     <div className='nav_wrapper d-flex align-items-center justify-content-between'>
