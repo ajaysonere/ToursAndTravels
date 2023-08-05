cf530b18703145413205a816384ea1e1c0587119
Ajay Sonere <ajaysonere786@gmail.com>
2023-08-05 16:35:05 +0530

    booking form is completed

--- tour-management/frontend/src/components/Booking/Booking.js
+++ tour-management/frontend/src/components/Booking/Booking.js
@@ -3,6 +3,10 @@ import './../../styles/booking.css';
 function Booking({tour , avgRating}){
 
     const {price , reviews} = tour;
+
+    const handleChange = e=>{
+
+    }
     return (
         <div className='booking'>
             <div className='booking_top d-flex align-items-center justify-content-between'>
@@ -11,6 +15,26 @@ function Booking({tour , avgRating}){
                                 <i class="ri-star-fill"></i>{avgRating === 0 ? null : avgRating}({reviews.length})
                             </span>
             </div>
+
+            {/* booking  form start */}
+            <div className='booking_form'>
+                <h5>Information</h5>
+                <Form className='booking_info-form'>
+                    <FormGroup>
+                        <input type='text' placeholder='Full Name' id = "fullname" required onChange={handleChange}/>
+                    </FormGroup>
+                    <FormGroup>
+                        <input type='number' placeholder='Phone Number' id = "phone" required onChange={handleChange}/>
+                    </FormGroup>
+                    <FormGroup className='d-flex align-items-center gap-3'>
+                        <input type='date' placeholder='Booking at' id = "fullname" required onChange={handleChange}/>
+                    </FormGroup>
+                    <FormGroup>
+                        <input type='number' placeholder='Guest size ' id = "fullname" required onChange={handleChange}/>
+                    </FormGroup>
+                </Form>
+            </div>
+            {/* booking  form end */}
         </div>
     );
 }
