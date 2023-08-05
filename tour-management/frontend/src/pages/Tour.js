4c47c44558985ca90b6dc1a319ac2f852dc87080
Ajay Sonere <ajaysonere786@gmail.com>
2023-08-05 11:21:56 +0530

    pagination is completed

--- tour-management/frontend/src/pages/Tour.js
+++ tour-management/frontend/src/pages/Tour.js
@@ -1,7 +1,59 @@
+
+import React , {useState , useEffect} from "react";
+import CommonSection from "../shared/CommonSection";
+import TourCard from '../shared/TourCard';
+import SearchBar from '../shared/SearchBar';
+import NewsLetter from '../shared/NewsLetter';
+import tourData from '../assets/data/tours';
+import '../styles/tour.css';
+import { Container, Row ,Col} from "reactstrap";
 function Tour(){
+
+    const [pageCount , setPageCount] = useState(0);
+    const [page , setPage] = useState(0);
+
+    useEffect(()=>{
+       const pages = Math.ceil(5/4);
+       setPageCount(pages);
+    } ,[page]);
+
     return (
         <>
-          <div>Tour</div>
+          <CommonSection title={'All Tours'}/>
+          <section>
+            <Container>
+              <Row>
+                <SearchBar />
+              </Row>
+            </Container>
+          </section>
+          <section className="pt-0">
+            <Container>
+              <Row>
+                 {
+                   tourData?.map(tour => (
+                     <Col lg='3' key={tour.id} className="mb-4">
+                      <TourCard tour={tour}/>
+                     </Col>
+                   ))
+                 }
+
+                 <Col lg='12'>
+                    <div className="pagination d-flex align-items-center justify-content-center mt-4 gap-3">
+                       {
+                         [...Array(pageCount).keys()].map(number=>(
+                           <span key={number} onClick={()=>setPage(number)}
+                           className={page ===number ? 'active_page' : ''}
+                           >
+                            {number+1}
+                           </span>
+                         ))
+                       }
+                    </div>
+                 </Col>
+              </Row>
+            </Container>
+          </section>
         </>
     );
 }
---
+++ tour-management/frontend/src/shared/CommonSection.js
@@ -0,0 +1,21 @@
+import './../styles/common-section.css';
+
+import {Container , Row, Col} from 'reactstrap';
+
+function CommonSection({title}){
+    return(
+       <>
+         <section className='common_section'>
+            <Container>
+                <Row>
+                    <Col lg='12'>
+                      <h1>{title}</h1>
+                    </Col>
+                </Row>
+            </Container>
+         </section>
+       </>
+    );
+}
+
+export default CommonSection;
