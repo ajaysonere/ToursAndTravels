08c879dc3f7c1dbb82dfe734d628ac2d07d982d2
Ajay Sonere <ajaysonere786@gmail.com>
2023-08-15 10:52:57 +0530

    added dotenv

--- tour-management/backend/index.js
+++ tour-management/backend/index.js
@@ -1,1 +1,13 @@
 // express server
+import  express  from "express";
+import dotenv from 'dotenv';
+import mongoose from "mongoose";
+import cors from 'cors';
+
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 3500;
+
+app.listen(process.env.PORT , ()=>{
+    console.log(`Server is running at http://localhost:${process.env.PORT}`);
+})
