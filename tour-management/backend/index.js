7cd3417c13f4bd2068d26a01962b484705e3638b
Ajay Sonere <ajaysonere786@gmail.com>
2023-08-15 12:38:26 +0530

    database connected

--- tour-management/backend/index.js
+++ tour-management/backend/index.js
@@ -3,11 +3,41 @@ import  express  from "express";
 import dotenv from 'dotenv';
 import mongoose from "mongoose";
 import cors from 'cors';
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 3500;
 
-app.listen(process.env.PORT , ()=>{
-    console.log(`Server is running at http://localhost:${process.env.PORT}`);
+// database connection
+const connect = async()=>{
+    try{
+       await mongoose.connect(process.env.MONGODB_URL , {
+          useNewUrlParser:true,
+          useUnifiedTopology:true,
+       });
+
+       console.log("Database is connected ");
+
+    }catch(err){
+       console.log(`Mongodb connection failed ${err}`);
+    }
+}
+
+
+
+// middleware
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
+
+
+// for testing
+app.get("/" , (req , res)=>{
+    res.send("API is Working ");
+})
+
+app.listen(port , ()=>{
+    connect();
+    console.log(`Server is running at http://localhost:${port}`);
 })
