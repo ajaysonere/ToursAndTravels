6f2b81c1cc5d4d1643b2d26906a1747f4b1b9c1c
Ajay Sonere <ajaysonere786@gmail.com>
2023-08-18 15:28:29 +0530

    tours routes are implemented

--- tour-management/backend/controllers/tourController.js
+++ tour-management/backend/controllers/tourController.js
@@ -3,8 +3,6 @@ import Tour from '../models/Tour.js'; // Make sure to import your Tour model her
 export const createTour = async (req, res) => {
     try {
         const newTour = new Tour(req.body);
-        console.log(newTour); // Debugging line, can be removed later
-
         const savedTour = await newTour.save();
         res.status(200).json({ success: true, message: 'Tour successfully created', data: savedTour });
     } catch (err) {
@@ -12,3 +10,45 @@ export const createTour = async (req, res) => {
         res.status(500).json({ success: false, message: 'Failed to create tour' });
     }
 };
+
+
+// update tour
+export const updateTour = async (req, res)=>{
+    try {
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ success: false, message: 'Failed to updating tour' });
+    }
+}
+
+// delete tour
+export const deleteTour = async (req, res)=>{
+    try {
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ success: false, message: 'Failed to deleting tour' });
+    }
+}
+
+// get songle tour
+export const getSingleTour = async (req, res)=>{
+    try {
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ success: false, message: 'Failed to get single tour' });
+    }
+}
+
+// get all tours
+export const getAllTour = async (req, res)=>{
+    try {
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ success: false, message: 'Failed to get all tours' });
+    }
+}
+
--- tour-management/backend/routers/tour.js
+++ tour-management/backend/routers/tour.js
@@ -1,9 +1,18 @@
 import express from 'express';
-import { createTour } from '../controllers/tourController.js';
+import { createTour, deleteTour, getAllTour, getSingleTour, updateTour } from '../controllers/tourController.js';
 
 const router = express.Router();
 
+// create a tour
 router.post('/', createTour);
+// update a tour
+router.put('/:id',updateTour);
+// delete a tour
+router.delete('/:id',deleteTour);
+// get a single tour details
+router.get('/:id' , getSingleTour);
+// get all tour details
+router.get('/' , getAllTour);
 
 
 
