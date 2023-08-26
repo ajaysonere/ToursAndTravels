b32828289f0dcb67e08673af3fe83cc0f69ec7a9
Ajay Sonere <ajaysonere786@gmail.com>
2023-08-26 13:44:43 +0530

    verify cookie implemented for user

---
+++ tour-management/backend/utils/verifyToken.js
@@ -0,0 +1,39 @@
+import jwt from 'jsonwebtoken';
+
+
+const verifyToken = (req, res,next)=>{
+    const token = req.cookies.accessToken;
+    
+    if(!token){
+       return res.status(401).json({
+           success:false,
+           message:"you are not authorized"
+       })
+    }
+
+    // if token is exist then verify the token 
+    jwt.verify(token , process.env.JWT_SECRET_KEY , (err , user)=>{
+        if(err){
+            return res.status(401).json({
+                success:false,
+                message:'Token is invalid'
+            })
+        }
+
+        req.user = user;
+        next();
+    })
+}
+
+export const verifyUser = (req , res,next)=>{
+    verifyToken(req,res,next, ()=>{
+        if(req.user.id === req.params.id || req.user.role === 'admin'){
+            next();
+        }else{
+            res.status(401).json({
+                success:false,
+                message:'you are not authenticated'
+            });
+        }
+    })
+}
