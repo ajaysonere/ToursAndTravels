bca2bd39a794051516f3f995e5c0786186af20e9
Ajay Sonere <ajaysonere786@gmail.com>
2023-08-27 21:45:02 +0530

    booking implemented

--- tour-management/backend/controllers/bookingController.js
+++ tour-management/backend/controllers/bookingController.js
@@ -1,5 +1,6 @@
 import booking from '../models/booking.js';
 
+// book tour
 export const createBooking = async(req,res)=>{
     const newBooking = new booking(req.body);
     try {
@@ -13,6 +14,42 @@ export const createBooking = async(req,res)=>{
         res.status(500).json({
             success:false,
             message:'Internal server error'
+        })
+    }
+}
+
+// get one booking
+export const getBooking = async(req,res)=>{
+    const id = req.params.id;
+    try {
+        const book = await booking.findById(id);
+        res.status(200).json({
+            success:true,
+            message:"Successful",
+            data:book
+        })
+    } catch (err) {
+        res.status(404).json({
+            success:false,
+            message:'Not found booking'
+        })
+    }
+}
+
+// get all booking
+
+export const getAllBooking = async(req,res)=>{
+    try {
+        const book = await booking.find();
+        res.status(200).json({
+            success:true,
+            message:"Successful",
+            data:book
+        })
+    } catch (err) {
+        res.status(404).json({
+            success:false,
+            message:'Not found bookings'
         })
     }
 }
