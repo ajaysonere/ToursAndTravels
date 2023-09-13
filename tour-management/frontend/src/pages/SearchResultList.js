1d93205c336d81f4ff2227e1da38dfa89fe219f1
Ajay Sonere <ajaysonere786@gmail.com>
2023-09-13 07:51:41 +0530

    search functionality added

--- tour-management/frontend/src/pages/SearchResultList.js
+++ tour-management/frontend/src/pages/SearchResultList.js
@@ -1,7 +1,29 @@
+import React , {useState} from 'react';
+import CommonSection from './../shared/CommonSection.js';
+import { Container , Row , Col} from 'reactstrap';
+import { useLocation } from 'react-router-dom';
+import TourCard from './../shared/TourCard.js';
+
 function SearchResultList(){
+    const location = useLocation();
+    const [data] = useState(location.state);
+    console.log(data);
     return(
       <>
-         <div>Search Result List</div>
+          <CommonSection title={'Tour search Result'} />
+          <section>
+            <Container>
+              <Row>
+                {
+                  data.length === 0? <h4>No tour found</h4>:data?.map(tour=>(
+                    <Col lg='3' className='mb-4' key={tour._id}>
+                      <TourCard tour={tour}/>
+                    </Col>
+                  ))
+                }
+              </Row>
+            </Container>
+          </section>
       </>
     );
 }
--- tour-management/frontend/src/shared/SearchBar.js
+++ tour-management/frontend/src/shared/SearchBar.js
@@ -1,20 +1,31 @@
-import '../styles/search-bar.css';
-import { Col , Form, FormGroup } from 'reactstrap';
 import React ,{useRef} from 'react';
+import { Col , Form, FormGroup } from 'reactstrap';
+import '../styles/search-bar.css';
+
+import {BASE_URL} from './../utils/config.js';
+import {useNavigate} from 'react-router-dom';
 
 function SearchBar(){
     const locationRef = useRef('');
     const distanceRef = useRef(0);
     const maxGroupSizeRef = useRef(0);
+    const navigate = useNavigate();
 
-    const searchHandler = ()=>{
+    const searchHandler = async()=>{
         const location = locationRef.current.value;
         const distance = distanceRef.current.value;
         const maxGroupSize = maxGroupSizeRef.current.value;
 
-        if(location === '' || distance === '' || maxGroupSize == ''){
+        if(location === '' || distance === '' || maxGroupSize === ''){
             return alert("All fields are required ! ");
         }
+
+        const res = await fetch(`${BASE_URL}/tours/search/getTourBySearch?city=${location}&distance=${distance}&maxGroupSize=${maxGroupSize}`)
+        if(!res.ok) alert('something went wrong')
+
+        const result = await res.json();
+
+        navigate(`/tours/search?city=${location}&distance=${distance}&maxGroupSize=${maxGroupSize}` , {state : result.data});
     }
     return (
        <>
