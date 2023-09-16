4428e1e66231e8799868418bcd5d975f24db2189
Ajay Sonere <ajaysonere786@gmail.com>
2023-09-16 13:53:23 +0530

    auth context updated

--- tour-management/frontend/src/index.js
+++ tour-management/frontend/src/index.js
@@ -7,12 +7,15 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "remixicon/fonts/remixicon.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import { AuthContextProvider } from "./context/AuthContext.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <AuthContextProvider>
+       <BrowserRouter>
+          <App />
+       </BrowserRouter>
+    </AuthContextProvider>
   </React.StrictMode>
 );
