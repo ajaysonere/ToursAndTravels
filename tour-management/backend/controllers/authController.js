2023b94c3ed34aa822782096f9c0316531a0d6c4
Ajay Sonere <ajaysonere786@gmail.com>
2023-09-26 23:06:56 +0530

    welcome email

--- tour-management/backend/controllers/authController.js
+++ tour-management/backend/controllers/authController.js
@@ -2,6 +2,7 @@ import Users from '../models/Users.js';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 
+
 export const register = async (req , res)=>{
 
     const salt = bcrypt.genSaltSync(10);
